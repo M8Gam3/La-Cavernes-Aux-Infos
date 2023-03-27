2db4d33605ed8b06ee0bca09af19d7c0363993d3
Yoann-Nicosia <yoann.nicosia@gmail.com>
2023-03-27 11:31:50 +0200

    ajout preferences responsive

--- xml/preferences.php
+++ xml/preferences.php
@@ -21,6 +21,8 @@ $preference = (json_decode($_COOKIE['preference']));
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <script defer src="./assets/JS/script.js"></script>
     <link rel="stylesheet" href="./assets/css/style.css">
+    <link rel="stylesheet" href="./assets/css/mediaqueries.css">
+
     <title>Préférences</title>
 </head>
 <body>
